4c532f1636380c213ccd63268277c0ad0774e84b
biojppm <joaoppmagalhaes@gmail.com>
2017-01-04 22:48:38 +0000

    make nose print test values

--- .ci/appveyor_test.bat
+++ .ci/appveyor_test.bat
@@ -3,6 +3,7 @@ echo on
 set cwd=%cd%
 echo Running in directory %cd%
 
-set PYTHONPATH=src
-%PYTHON%\\python.exe -m nose -v --with-id --nocapture
+set PYTHONPATH=%cwd%\src
+
+%PYTHON%\python.exe -m nose -d -v --with-id --nocapture
 
