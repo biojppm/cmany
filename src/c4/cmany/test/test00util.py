398c083cbe83c00fdce60bdb7553cd4e54cbad3e
biojppm <joaoppmagalhaes@gmail.com>
2017-01-06 10:28:27 +0000

    test util: minor formatting changes

--- src/c4/cmany/test/test00util.py
+++ src/c4/cmany/test/test00util.py
@@ -18,8 +18,6 @@ class Test00splitesc(ut.TestCase):
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-
-
 class Test01runsyscmd(ut.TestCase):
 
     def test00_noargs(self):
@@ -38,22 +36,25 @@ class Test01runsyscmd(ut.TestCase):
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-
 echo_args = None
 def invoke_and_compare(tester, cmd_args):
     cmd = [sys.executable, sys.modules[__name__].__file__, 'echo'] + cmd_args
-    out = util.runsyscmd(cmd, capture_output=True)
+    out = util.runsyscmd(cmd, echo_cmd=False, capture_output=True)
     # print("invoke_and_compare: out=", out)
     # this doesn't work...:
     # code = "echo_args={}".format(out)
     # ... but this does:
-    code = """tmp = {}
-setattr(sys.modules[__name__], 'echo_args', tmp)""".format(out)
+    code = "tmp = out; setattr(sys.modules[__name__], 'echo_args', tmp)"
+    code = code.format(out)
     # print("invoke_and_compare: code=", code)
     exec(code)
     # print("invoke_and_compare: echo_args=", echo_args)
     tester.assertEqual(echo_args, cmd_args)
 
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'echo':
         print(sys.argv[2:])  # for test_invoke()
