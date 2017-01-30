c92482ee225a5402f032b5cf63e6ae63ca23d501
biojppm <joaoppmagalhaes@gmail.com>
2017-01-30 20:53:58 +0000

    fixing appveyor test script

--- .ci/appveyor_test.bat
+++ .ci/appveyor_test.bat
@@ -1,10 +1,14 @@
 echo on
 
+cd orig=%cd%
 cd %0\..\..
 set root=%cd%
 cd test
 
 set PYTHON=%PYTHON%\python.exe
 call %root%\test\run.bat
+set stat=%ERRORLEVEL%
 
-exit /b %ERRORLEVEL%
+cd %orig%
+
+exit /b %stat%
