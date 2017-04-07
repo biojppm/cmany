66631a0b3f782bd1939a243fbd32f61f0374e667
Joao Paulo Magalhaes <dev@jpmag.me>
2017-04-07 14:23:33 +0100

    improve dev test script

--- .ci/dev_test.sh
+++ .ci/dev_test.sh
@@ -14,6 +14,14 @@ function show_cmd()
     $*
 }
 
+function mark_failed()
+{
+    stat=$1
+    ver=$2
+    stat=$1
+    failed="$failed $ver"
+}
+
 for v in $PYENV_VERSIONS ; do
     pyenv local $v || exit 1
     show_cmd which python
@@ -22,17 +30,19 @@ for v in $PYENV_VERSIONS ; do
     show_cmd pyenv which pip
     pip install -r $rdir/requirements.txt
     pip install -r $rdir/requirements_test.txt
-    $sdir/../test/run.sh $*
-    if [ $? != 0 ] ; then
-        stat=$?
-        failed="$failed $v"
-    fi
+    $sdir/../test/run.sh $* || mark_failed $? $v
 done
 
 if [ $stat == 0 ] ; then
-    echo "$0: Successfully ran all tests: $PYENV_VERSIONS"
+    echo "$0: Successfully ran all tests:"
+    for v in $PYENV_VERSIONS ; do
+        echo $v
+    done
 else
-    echo "$0: failed tests: $failed"
+    echo "$0: ----- FAILED tests:"
+    for v in $failed ; do
+        echo $v
+    done
 fi
 
 exit $stat
