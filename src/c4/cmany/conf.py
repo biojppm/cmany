583c0a87df09d60bba1b3dd8fcc15b4ee1819503
Joao Paulo Magalhaes <dev@jpmag.me>
2017-04-10 22:25:04 +0100

    conf WIP: add Configs class

--- src/c4/cmany/conf.py
+++ src/c4/cmany/conf.py
@@ -1,11 +1,37 @@
 import os.path as osp
 
 CONF_DIR = osp.join(osp.dirname(__file__), "conf")
+# maybe cmany is not installed. Then it must be a source distribution.
 if not osp.exists(CONF_DIR):
     CONF_DIR = osp.abspath(osp.join(osp.dirname(__file__), "../../../conf"))
     assert osp.exists(CONF_DIR)
 
 USER_DIR = osp.expanduser("~/.cmany/")
 
-CONF_FLAGS_FILE = osp.join(CONF_DIR, 'flags.yml')
-USER_FLAGS_FILE = osp.join(USER_DIR, 'flags.yml')
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+class Configs:
+
+    @staticmethod
+    def load(file_seq=None):
+        curr = None
+        for fn in file_seq:
+            if not osp.exists(fn):
+                continue
+            if curr is None:
+                curr = Configs(fn)
+            else:
+                tmp = Configs(fn)
+                curr.merge_from(tmp)
+        return curr
+
+    def __init__(self, filename):
+        with open(filename, "r") as f:
+            txt = f.read()
+        from . import flags as c4flags
+        self.flag_aliases = c4flags.FlagAliases(yml=txt)
+
+    def merge_from(self, other):
+        self.flag_aliases = self.flag_aliases.merge_from(other.flag_aliases)
