1362d058716fe6b6ed91b6066bbd14d4c58da33e
Joao Paulo Magalhaes <dev@jpmag.me>
2017-04-13 16:18:21 +0100

    unit tests for combinations WIP

--- test/test04combinations.py
+++ test/test04combinations.py
@@ -8,70 +8,110 @@ from collections import OrderedDict as odict
 import c4.cmany as cmany
 from c4.cmany import util, args as c4args
 from c4.cmany import variant
-from c4.cmany import build_item
+from c4.cmany.project import CombinationRules
 
+# utility variables for simpler names
+ds = cmany.System.default_str()
+da = cmany.Architecture.default_str()
+dc = cmany.Compiler.default_str()
+dt = cmany.BuildType.default_str()
+dv = cmany.Variant.default_str()
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 class Test10AsArguments(ut.TestCase):
 
-    @staticmethod
-    def count(subject):
-        c = 1
-        for i in ('systems', 'architectures', 'compilers', 'build_types', 'variants'):
-            if not isinstance(subject, odict):
-                ai = getattr(subject, i)
-            else:
-                ai = subject.get(i)
-            c = c * len(ai)
-        return c
+    def t(self, input, expected_items, expected_combinations):
+        if not hasattr(self, 'parser'):
+            parser = argparse.ArgumentParser()
+            c4args.add_select(parser)
+            c4args.add_bundle_flags(parser)
 
-    def t(self, input, expected_items):
-        parser = argparse.ArgumentParser()
-        c4args.add_select(parser)
-        c4args.add_bundle_flags(parser)
         args = parser.parse_args(input)
+        print(args)
         assert isinstance(expected_items, odict)
 
-        expected_count = __class__.count(expected_items)
-        actual_count = __class__.count(args)
-        with self.subTest(input=input, msg="expected count"):
-            self.assertEqual(actual_count, expected_count)
+        for i in ('systems', 'architectures', 'compilers', 'build_types', 'variants'):
+            expected = expected_items[i]
+            actual = getattr(args, i)
+            util.lognotice(i, "actual=", actual)
+            util.lognotice(i, "expected=", expected)
+            with self.subTest(msg="expected count of "+i, input=input):
+                self.assertEqual(len(actual), len(expected))
+            with self.subTest(msg="expected set of "+i, input=input):
+                self.assertEqual(set(actual), set(expected))
 
-        # for k, refval in expected_props.items():
-        #     result = getattr(var.flags, k)
-        #     with self.subTest(input=input, which_var=which_var, desc='variant properties', prop=k):
-        #         self.assertEqual(result, refval, msg='property: ' + k)
+        cr = []
+        if hasattr(args, 'combination_rules'):
+            cr = args.combination_rules
+        cr = CombinationRules(cr)
+        items = cr.valid_combinations(
+            args.systems, args.architectures, args.compilers,
+            args.build_types, args.variants)
 
     def test00(self):
-        self.t([],
-               odict([
-                   ('systems', [cmany.System.default()]),
-                   ('architectures', [cmany.Architecture.default()]),
-                   ('compilers', [cmany.Compiler.default()]),
-                   ('build_types', [cmany.BuildType.default()]),
-                   ('variants', [])
-               ]))
+        self.t(
+            # args
+            [],
+            # expected items
+            odict([
+                ('systems', [ds]),
+                ('architectures', [da]),
+                ('compilers', [dc]),
+                ('build_types', [dt]),
+                ('variants', [dv])
+            ]),
+            # expected combinations
+            [
+                (ds, da, dc, dt, dv),
+            ]
+        )
 
-        self.t(["-a", "x86,x86_64", "-v", "none,'foo: -X wall','bar: -D BAR_DEF=1'"],
-               odict([
-                   ('systems', [cmany.System.default()]),
-                   ('architectures', ['x86', 'x86_64']),
-                   ('compilers', [cmany.Compiler.default()]),
-                   ('build_types', [cmany.BuildType.default()]),
-                   ('variants', ['none', 'foo', 'bar'])
-               ]))
+    def test01(self):
+        self.t(
+            # args
+            ["-a", "x86,x86_64", "-v", "none,'foo: -X wall','bar: -D BAR_DEF=1'"],
+            # expected items
+            odict([
+                ('systems', [ds]),
+                ('architectures', ['x86', 'x86_64']),
+                ('compilers', [dc]),
+                ('build_types', [dt]),
+                ('variants', ['none', 'foo', 'bar'])
+            ]),
+            # expected combinations
+            [
+                (ds, 'x86', dc, dt, 'none'),
+                (ds, 'x86', dc, dt, 'foo'),
+                (ds, 'x86', dc, dt, 'var'),
+                (ds, 'x86_64', dc, dt, 'none'),
+                (ds, 'x86_64', dc, dt, 'foo'),
+                (ds, 'x86_64', dc, dt, 'var'),
+            ]
+        )
 
-        return
-        self.t(["-a", "x86,x86_64", "-v", "none,'foo: -X wall --exclude-all 64','bar: -D BAR_DEF=1 --exclude-all 64'"],
-               odict([
-                   ('systems', [cmany.System.default()]),
-                   ('architectures', ['x86', 'x86_64']),
-                   ('compilers', [cmany.Compiler.default()]),
-                   ('build_types', [cmany.BuildType.default()]),
-                   ('variants', ['none', 'foo'])
-               ]))
+        self.t(
+            # args
+            ["-a", "x86,x86_64", "--exclude-all", "foo.*64.*", "-v", "none,'foo: -X wall','bar: -D BAR_DEF=1 --exclude-all 64'"],
+            # expected items
+            odict([
+                ('systems', [ds]),
+                ('architectures', ['x86', 'x86_64']),
+                ('compilers', [dc]),
+                ('build_types', [dt]),
+                ('variants', ['none', 'foo', 'bar'])
+            ]),
+            # expected combinations
+            [
+                (ds, 'x86', dc, dt, 'none'),
+                (ds, 'x86', dc, dt, 'foo'),
+                (ds, 'x86', dc, dt, 'var'),
+                (ds, 'x86_64', dc, dt, 'none'),
+                #(ds, 'x86_64', dc, dt, 'foo'), # excluded
+                (ds, 'x86_64', dc, dt, 'var'),
+            ]
+        )
 
 
 # -----------------------------------------------------------------------------
