6d48037ec73b022ded598f81b7491170d9945e15
Joao Paulo Magalhaes <dev@jpmag.me>
2017-04-26 13:59:31 +0100

    improve unit tests for BuildFlags

--- test/test07config.py
+++ test/test07config.py
@@ -12,7 +12,7 @@ from c4.cmany.build_flags import BuildFlags
 
 from ruamel import yaml as yaml
 from ruamel.yaml.comments import CommentedMap as CommentedMap
-
+import types
 
 # -----------------------------------------------------------------------------
 def tmpfile(contents):
@@ -118,104 +118,151 @@ class Test01ConfigsFile(ut.TestCase):
         self.assertEqual(c.get_val("foo.bar.baz"), 10)
         self.assertEqual(c.get_val("foo.bar.askdjaksjd"), 11)
 
+
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-class BFTCase:
-    @property
-    def name(self):
-        return self.__class__.__name__[7:].lower()
+class ConfFixtureBase(ut.TestCase):
+    """base class for config test cases"""
+
     def do_load(self):
-        cm = yaml.load(self.__class__.yml, yaml.RoundTripLoader)
-        if cm is None: cm = CommentedMap()
+        cm = self.cm.copy()
         bf = BuildFlags("")
         bf.load_config(cm)
         return bf
+
     def do_save(self):
         cm = CommentedMap()
-        self.__class__.bf.save_config(cm)
+        self.bf.save_config(cm)
         return cm
+
     def do_roundtrip_save_load(self):
         cm = CommentedMap()
-        self.__class__.bf.save_config(cm)
+        self.bf.save_config(cm)
         bf = BuildFlags("")
         bf.load_config(cm)
         return bf
+
     def do_roundtrip_load_save(self):
-        cm = yaml.load(self.__class__.yml, yaml.RoundTripLoader)
-        if cm is None: cm = CommentedMap()
         bf = BuildFlags("")
-        bf.load_config(cm)
+        bf.load_config(self.cm)
         cm = CommentedMap()
         bf.save_config(cm)
         return cm
-    def check_bf(self, tstobj, bf):
-        with tstobj.subTest(name=self.name, msg=self.name):
-            self._check_bf(tstobj, bf)
-    def check_cm(self, tstobj, cm):
-        with tstobj.subTest(name=self.name, msg=self.name):
-            self._check_cm(tstobj, cm)
 
-
-class BFTCaseEmpty(BFTCase):
-    bf = BuildFlags("empty")
-    yml = ""
-    def _check_bf(self, tstobj, bf):
-        for a in BuildFlags.attrs:
-            tstobj.assertTrue(not getattr(bf, a))
-    def _check_cm(self, tstobj, cm):
-        tstobj.assertTrue(not cm)
-
-
-class BFTCaseCMakeVars(BFTCase):
-    bf = BuildFlags("cmake_vars", cmake_vars=["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"])
-    yml = """\
-cmake_vars: VARFOO=BAR CMAKE_VERBOSE_MAKEFILE=1
-"""
-    non_empty = ("cmake_vars",)
-
-    def _check_bf(self, tstobj, bf):
-        tstobj.assertEqual(getattr(bf, "cmake_vars"), ["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"])
-        for a in BuildFlags.attrs:
-            if not a in __class__.non_empty:
-                tstobj.assertTrue(not getattr(bf, a))
-
-    def _check_cm(self, tstobj, cm):
-        a = cm["cmake_vars"]
-        a = BuildFlags.flag_str_to_list(a)
-        tstobj.assertEqual(a, ["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"])
-        for a in BuildFlags.attrs:
-            if not a in __class__.non_empty:
-                tstobj.assertTrue(not hasattr(cm, a))
-
-
-bf_cases = (BFTCaseEmpty(), BFTCaseCMakeVars())
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
-class Test02BuildFlags(ut.TestCase):
+# Test cases for build flags
+
+# using the _ prefix prevents unittest from instantiating tests for this class
+class _BFTCase(ConfFixtureBase):
+    """base for build flags test case"""
+
+    def _setUp(self, bf, yml):
+        self.bf = bf
+        self.yml = yml
+        self.name = self.__class__.__name__[7:].lower()
+        self.cm = yaml.load(self.yml, yaml.RoundTripLoader)
+        if self.cm is None:
+            self.cm = CommentedMap()
+        self.non_empty = []
+        for a in BuildFlags.attrs:
+            if getattr(bf, a):
+                self.non_empty.append(a)
+
+    def check_bf(self, bf):
+        with self.subTest(name=self.name, msg=self.name):
+            for a in self.non_empty:
+                self.assertEqual(getattr(bf, a), getattr(self.bf, a))
+            for a in BuildFlags.attrs:
+                if a not in self.non_empty:
+                    self.assertTrue(not getattr(bf, a))
+
+    def check_cm(self, cm):
+        with self.subTest(name=self.name, msg=self.name):
+            for a in self.non_empty:
+                v = cm[a]
+                if a != 'toolchain':
+                    v = BuildFlags.flag_str_to_list(v)
+                self.assertEqual(v, getattr(self.bf, a))
+            for a in BuildFlags.attrs:
+                if a not in self.non_empty:
+                    self.assertTrue(not hasattr(cm, a))
 
     def test01Save(self):
-        for cls in bf_cases:
-            cm = cls.do_save()
-            cls.check_cm(self, cm)
+        cm = self.do_save()
+        self.check_cm(cm)
 
     def test02Load(self):
-        for cls in bf_cases:
-            bf = cls.do_load()
-            cls.check_bf(self, bf)
+        bf = self.do_load()
+        self.check_bf(bf)
 
     def test03RoundtripSaveLoad(self):
-        for cls in bf_cases:
-            bf = cls.do_roundtrip_save_load()
-            cls.check_bf(self, bf)
+        bf = self.do_roundtrip_save_load()
+        self.check_bf(bf)
 
     def test04RoundtripLoadSave(self):
-        for cls in bf_cases:
-            cm = cls.do_roundtrip_load_save()
-            cls.check_cm(self, cm)
+        cm = self.do_roundtrip_load_save()
+        self.check_cm(cm)
 
+
+# -----------------------------------------------------------------------------
+class Test02BuildFlags01Empty(_BFTCase):
+    def setUp(self):
+        self._setUp(bf=BuildFlags("empty"), yml="")
+
+
+class Test02BuildFlags02CMakeVars(_BFTCase):
+    def setUp(self):
+        super()._setUp(
+            bf=BuildFlags("cmake_vars",
+                          cmake_vars=["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"]),
+            yml="""\
+cmake_vars: VARFOO=BAR CMAKE_VERBOSE_MAKEFILE=1
+""")
+
+
+class Test02BuildFlags03CMakeVarsAndCxxflags(_BFTCase):
+    def setUp(self):
+        self._setUp(
+            bf=BuildFlags("cmake_vars_and_cxxflags",
+                          cmake_vars=["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"],
+                          cxxflags=["-std=c++11", "-Wall", ]),
+            yml="""\
+cmake_vars: VARFOO=BAR CMAKE_VERBOSE_MAKEFILE=1
+cxxflags: -std=c++11 -Wall
+""")
+
+
+class Test02BuildFlags04Full(_BFTCase):
+    def setUp(self):
+        self._setUp(
+            bf=BuildFlags("cmake_vars_and_cxxflags",
+                          cmake_vars=["VARFOO=BAR", "CMAKE_VERBOSE_MAKEFILE=1"],
+                          defines=["FOO", "BAR", "VFOO=baaaaaaaaaa"],
+                          cflags=["-std=c++11", "-Wall", ],
+                          cxxflags=["-std=c++11", "-Wall", ],
+                          toolchain="somefile.cmake"),
+            yml="""\
+cmake_vars: VARFOO=BAR CMAKE_VERBOSE_MAKEFILE=1
+defines: FOO BAR VFOO=baaaaaaaaaa
+cflags: -std=c++11 -Wall
+cxxflags: -std=c++11 -Wall
+toolchain: somefile.cmake
+""")
+
+
+def suite():
+    suite = ut.TestSuite()
+    suite.addTest(Test02BuildFlags01Empty())
+    suite.addTest(Test02BuildFlags02CMakeVars())
+    suite.addTest(Test02BuildFlags03CMakeVarsAndCxxflags())
+    suite.addTest(Test02BuildFlags04Full())
+    return suite
+
+suite()
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
