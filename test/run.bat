7819304a07d6aa495c95287ac3b4190b26d6bde5
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-07 17:41:02 +0100

    test that cmany can be installed

--- test/run.bat
+++ test/run.bat
@@ -8,7 +8,34 @@ set PYTHONPATH=%root%\src
 
 if not defined PYTHON set PYTHON=python3
 if "%PYTHON%" == "" set PYTHON=python3
+if not defined PIP set PIP="pip3 --user"
+if "%PIP%" == "" set PYTHON="pip3 --user"
 
 %PYTHON% -m nose -d -v --with-id --nocapture %*
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cd %root%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+if exist dist (
+   del dist/cmany-*.whl
+)
+%PYTHON% setup.py sdist bdist_wheel
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+%PIP% install --user dist/cmany-*.whl
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+%PIP% show -f cmany
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmany h
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+cmany h quick_tour
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+%PIP% uninstall cmany
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 exit /b %ERRORLEVEL%
--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
 set -x
+set -e
 
 cwd=$(cd $(dirname $0) ; pwd)
 export PYTHONPATH=$cwd/../src
 PY=${PYTHON:-python3}
+PIP=${PIP:-pip3}
 
 $PY -m nose -d -v --with-id --nocapture $*
+
+# test that cmany can be installed and ran
+cd $cwd/..
+if [ -d dist ] ; then
+    rm -vf dist/cmany-*
+fi
+export PATH=$PATH:$HOME/.local/bin
+$PY setup.py sdist bdist_wheel
+$PIP uninstall --user cmany || echo ""
+$PIP install --user dist/cmany-*.whl
+$PIP show -f cmany
+cmany h
+cmany h quick_tour
+$PIP uninstall cmany
 
 exit $?
